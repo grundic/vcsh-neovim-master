185a822070b2dcaaa7cff4369dc7ff483bea9e8b
Grigory Chernyshev <systray@yandex.ru>
2022-01-19 19:56:16 +0000

    Add several usefull autocommands.

--- .config/nvim/lua/settings/60-autocommands.lua
+++ .config/nvim/lua/settings/60-autocommands.lua
@@ -2,9 +2,42 @@
 --]]
 local cmd = vim.cmd
 
--- Restore cursor position after reopening file.
 cmd([[
+  " Restore cursor position after reopening file.
   autocmd BufRead * autocmd FileType <buffer> ++once
       \ if &ft !~# 'commit\|rebase' && line("'\"") > 1 && line("'\"") <= line("$") | exe 'normal! g`"' | endif
+
+  " Highlight trailing spaces.
+  highlight ExtraWhitespace ctermbg=red guibg=red
+  match ExtraWhitespace /\s\+$/
+  autocmd BufWinEnter * match ExtraWhitespace /\s\+$/
+  autocmd InsertEnter * match ExtraWhitespace /\s\+\%#\@<!$/
+  autocmd InsertLeave * match ExtraWhitespace /\s\+$/
+  autocmd BufWinLeave * call clearmatches()
+
+  " Trim trailing spaces automatically.
+  autocmd BufWritePre * lua strip_trailing_whitespace()
+
+  " Turn of relative numbers and hightlighting for inactive window and insert mode.
+  :augroup numbertoggle
+  :  autocmd!
+  :  autocmd BufEnter,FocusGained,InsertLeave * setlocal relativenumber cursorline
+  :  autocmd BufLeave,FocusLost,InsertEnter   * setlocal norelativenumber nocursorline
+  :augroup END
+
+  " Highlight on yank.
+  augroup YankHighlight
+    autocmd!
+    autocmd TextYankPost * silent! lua vim.highlight.on_yank({timeout = 200})
+  augroup end
 ]])
 
+function strip_trailing_whitespace()
+  if vim.bo.modifiable then
+    local line = vim.fn.line('.')
+    local col = vim.fn.col('.')
+    vim.cmd([[%s/\s\+$//e]])
+    vim.fn.histdel('/', -1)
+    vim.fn.cursor(line, col)
+  end
+end
