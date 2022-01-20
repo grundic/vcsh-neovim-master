01d2039bc633c700bc814ad5343d0043d6eff4a3
Grigory Chernyshev <systray@yandex.ru>
2022-01-20 09:43:20 +0000

    Add plugins for LSP support.

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -12,6 +12,11 @@ return require('packer').startup(function()
   -- Parsing your code into a syntax tree allows for manipulating the structure of your code in a more intelligent way
   use {'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'}
 
+  -- Language Server Protocol.
+  use 'neovim/nvim-lspconfig'
+
+  use {'nvim-lua/lsp-status.nvim', requires = 'neovim/nvim-lspconfig'}
+
   -- Hop is an EasyMotion-like plugin allowing you to jump anywhere in a document with as few keystrokes as possible.
   use 'phaazon/hop.nvim'
 
