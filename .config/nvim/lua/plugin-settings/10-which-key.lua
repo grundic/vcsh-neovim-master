90301be08bbbe8e91a1c3165fb387665595783cb
Grigory Chernyshev <systray@yandex.ru>
2024-03-29 17:39:32 +0100

    Display WhichKey in various modes.

--- .config/nvim/lua/plugin-settings/10-which-key.lua
+++ .config/nvim/lua/plugin-settings/10-which-key.lua
@@ -1,3 +1,9 @@
 require('which-key').setup({
   -- one can check for conflicting key-bidings with `checkhealth which_key`.
+
+  -- https://www.reddit.com/r/vim/comments/vwhdpa/way_to_list_internalbuiltin_vim_keybindings/
+  vim.api.nvim_set_keymap('n', '<M-k>', '<cmd>WhichKey<cr>', { noremap = true, silent = true, desc = "Display WhichKey for normal mode." }),
+  vim.api.nvim_set_keymap('v', '<M-k>', '<cmd>WhichKey \'\' v<cr>', { noremap = true, silent = true, desc = "Display WhichKey for visual mode." }),
+  vim.api.nvim_set_keymap('i', '<M-k>', '<cmd>WhichKey \'\' i<cr>', { noremap = true, silent = true, desc = "Display WhichKey for insert mode." }),
+  vim.api.nvim_set_keymap('c', '<M-k>', '<cmd>WhichKey \'\' c<cr>', { noremap = true, silent = true, desc = "Display WhichKey for command mode." }),
 })
